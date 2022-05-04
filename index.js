68fdbbac2b38d055834edb5e885825e6373f0f06
Gerardo <gerardo.rosas@external.danone.com>
2022-05-03 21:57:43 -0500

    commit init

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+var n = 0;
+var x = 5;
+
+while (n < x) {
+    console.log("resultado n: ", n);
+    n = n + 1;
+}
+console.log("--------------------------")
+var n2 = 0;
+var x2 = 6;
+
+do {
+    console.log("resultado: ", n2);
+    n2 = n2 + 1;
+} while (n2 < x2);
+
+console.log("---------------------------")
+
+var n3 = 1;
+var tabla = 5;
+do {
+    var result = n3 * tabla;
+    console.log("resultado: " + n3 + " X " + tabla + " = " + result );
+    n3 = n3 + 1;
+} while (n3 < 11);
+
+
+
+
+
+
+
+
+
+
+
